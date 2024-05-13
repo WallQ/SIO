b178be94c7447fc8022aae3fda3ccbe5ffc6a743
Sérgio Félix <sergio98@outlook.pt>
2024-05-13 19:45:37 +0100

    chore: Update drizzle-kit dependency to version 0.21.1 and refactor database configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,9 +3,9 @@ import { type Config } from 'drizzle-kit';
 
 export default {
 	schema: './src/server/db/schema.ts',
-	driver: 'pg',
+	dialect: 'postgresql',
 	dbCredentials: {
-		connectionString: env.DATABASE_URL,
+		url: env.DATABASE_URL,
 	},
 	tablesFilter: ['sio_*'],
 } satisfies Config;
